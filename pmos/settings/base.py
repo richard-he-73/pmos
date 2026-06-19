b8289fadb79f5815191016868e44c7545271e2bd
Richard.He <hefeng.dccbj@gmail.com>
2026-06-19 22:03:31 +0800

    security: enable Django password validators (min 8 chars, no common/numeric pwd)

--- pmos/settings/base.py
+++ pmos/settings/base.py
@@ -82,7 +82,11 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = []
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', 'OPTIONS': {'min_length': 8}},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
 
 AUTH_USER_MODEL = 'accounts.User'
 
